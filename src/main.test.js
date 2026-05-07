585934462d56b28a4c4ca19c32aa312937c325e0
nadiiarybak <nadiia.rybak@gmail.com>
2026-05-06 21:28:18 -0400

    Hello, world!

--- src/main.test.js
+++ src/main.test.js
@@ -18,9 +18,9 @@ const getSiteBody = (startWord, finishWord) => {
     lastIndex + finishWord.length,
   );
 
-  const siteBody = childProcess.execSync(
-    `curl ${url}`,
-  ).toString();
+  // const siteBody = childProcess.execSync(
+  //   `curl ${url}`,
+  // ).toString();
 
   return siteBody;
 };

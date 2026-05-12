114c05c58eb82bc6b69b368ed0c14e4c3674d43c
nadiiarybak <nadiia.rybak@gmail.com>
2026-05-12 17:42:21 -0400

    Hello, world!

--- src/main.test.js
+++ src/main.test.js
@@ -23,7 +23,6 @@ const getSiteBody = (startWord, finishWord) => {
   ).toString();
 
   return siteBody;
-
 };
 
 describe('Environmental Check', () => {
@@ -146,17 +145,17 @@ describe('Environmental Check', () => {
       .toBeTruthy();
   });
 
-  // test(`You should deploy test page to GitHub pages`, () => {
-  //   if (OS === 'Workflow') {
-  //     const testLinkBody = getSiteBody('[TEST REPORT LINK]', '_report/');
-  //
-  //     expect(testLinkBody)
-  //       .toContain('BackstopJS Report');
-  //   }
-  //
-  //   expect(true)
-  //     .toBeTruthy();
-  // });
+  test(`You should deploy test page to GitHub pages`, () => {
+    if (OS === 'Workflow') {
+      const testLinkBody = getSiteBody('[TEST REPORT LINK]', '_report/');
+
+      expect(testLinkBody)
+        .toContain('BackstopJS Report');
+    }
+
+    expect(true)
+      .toBeTruthy();
+  });
 
   test('You should have Google Chrome or Firefox', () => {
     if (OS === 'Windows') {
